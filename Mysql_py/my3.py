ea6ff8f0cb6ae8e186ea5e7970038aacc00b4620
omariba <omariba5@gmail.com>
2017-03-29 08:32:51 +0300

    a simple read and insert into db script

--- Mysql_py/my3.py
+++ Mysql_py/my3.py
@@ -1,10 +1,25 @@
 import MySQLdb
 
-con = MySQLdb.connect("localhost","root","toor","pyclass")
+choice = 0
+while choice != 1 and choice !=2:
+	choice = input("Type 1 to read from database\nType 2 to insert into database\n")
+	con = MySQLdb.connect("localhost","root","toor","pyclass")
 
-place = con.cursor()
-query = "insert into students(`Admin No`,Fname,Lname)values(1,'Genius','Persons');"
-place.execute(query)
-con.commit()
+	place = con.cursor()
 
-con.close()
+	if choice == 1:
+		query = "select * from students"
+		place.execute(query)
+		data = place.fetchall()
+		for y in data:
+			print y
+		con.close()
+	elif choice == 2:
+		# query = "insert into students(`Admin No`,Fname,Lname)values(1,'Genius','Persons');"
+		query = raw_input("enter your query here: ")
+		place.execute(query)
+		con.commit()
+		con.close()
+	else:
+		print "INVALID CHOICE"
+		con.close()
