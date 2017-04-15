8b8348096d803ce99a53676093725e2002fc9316
omariba <omariba5@gmail.com>
2017-04-15 20:07:00 +0300

    resumed django

---
+++ Django/C/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/root/Desktop/Nelly/Programming/Python/Python_Stff/Django/C/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
