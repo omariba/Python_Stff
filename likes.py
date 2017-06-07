ba25b3188ac1c936cd1ac9deb505ce717230c269
omariba <omariba5@gmail.com>
2017-06-07 15:59:24 +0300

    Codewars Kata

---
+++ likes.py
@@ -0,0 +1,12 @@
+def likes(names):
+    if len(names) == 0:
+        return "no one likes this"
+    elif len(names) == 1:
+        return "%s likes this" %names[0]
+    elif len(names) == 2:
+        return "%s and %s like this" %(names[0],names[1])
+    elif len(names) == 3:
+        return "%s, %s and %s like this" %(names[0],names[1],names[2])
+    elif len(names) > 3:
+        others = len(names) - 2
+        return "%s, %s and %s others like this" %(names[0],names[1],others)
