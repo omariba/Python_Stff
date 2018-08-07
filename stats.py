3bb7f443ee12afeb51bd086335d95909601b10c9
omariba <omariba5@gmail.com>
2018-08-07 20:50:42 +0300

    pvariance

--- stats.py
+++ stats.py
@@ -1,7 +1,14 @@
 import statistics
 
 nums = [12,33,2,4,8,9]
+names = ["pup","nun","kun","sus","lul","nun"]
 
-ans = statistics.median(nums)
-
-print (ans) 
+try:
+    statistics.median(nums)
+    print(statistics.mode(nums))
+    ans = statistics.pvariance(nums)
+    # print(ans) 
+    # print(statistics.mean(nums))
+    # print(sum(nums))
+except ValueError:
+    print('None found')
