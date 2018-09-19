a7318aff4d7a4bba0de03ba0baa1735ea032addd
omariba <omariba5@gmail.com>
2018-09-19 18:31:02 +0300

    board generated

--- Google_Challenges/chess_board.py
+++ Google_Challenges/chess_board.py
@@ -1,7 +1,13 @@
 board = []
-s = 0
-d = 8
+b = 0
+e = 8
 for i in range(8):
-	board.append(range(s,d))
-	s = d
-	d += 8
+    if e <= 71:
+		board.append(range(b,e))
+		b = e
+		e += 8
+
+# if 4 in board[0:1][0]:
+#     print board[0:1][0].index(4)
+
+print board
